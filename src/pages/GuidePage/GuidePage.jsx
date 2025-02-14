b2eaee91655fe2436671dc2304a171363de6b169
Amy <xyl.amy@gmail.com>
2025-02-13 20:11:42 -0800

    minor changes

--- src/pages/GuidePage/GuidePage.jsx
+++ src/pages/GuidePage/GuidePage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import GuidePageCard from "../components/GuidePageCard";
+import GuidePageCard from "../components/GuidePageCard/GuidePageCard";
 
 function GuidePage() {
   const { toolId } = useParams();
