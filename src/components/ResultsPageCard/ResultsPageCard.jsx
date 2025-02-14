1b1657a602fa4bfd0909dcc95c2c8604e69c15e6
Amy <xyl.amy@gmail.com>
2025-02-13 22:04:22 -0800

    change results to result

--- src/components/ResultsPageCard/ResultsPageCard.jsx
+++ src/components/ResultsPageCard/ResultsPageCard.jsx
@@ -3,25 +3,25 @@ import { Link } from "react-router-dom";
 
 function ResultsPageCard({ tool, toolId }) {
   return (
-    <article className="results-card">
-      <div className="results-card__logo">
+    <article className="result-card">
+      <div className="result-card__logo">
         <img
           src={tool.logo_url}
           alt={`${tool.tool} logo`}
-          className="results-card__logo-img"
+          className="result-card__logo-img"
         />
       </div>
-      <h2 className="results-card__header">{tool.tool}</h2>
-      <p className="results-card__description">{tool.description}</p>
-      <div className="results-card__buttons">
-        <Link to={`/guide/${toolId}}`} className="results-card__btn">
+      <h2 className="result-card__header">{tool.tool}</h2>
+      <p className="result-card__description">{tool.description}</p>
+      <div className="result-card__buttons">
+        <Link to={`/guide/${toolId}}`} className="result-card__btn">
           Learn More
         </Link>
         <a
           href={tool.url}
           target="_blank"
           rel="noopener noreferrer"
-          className="results-card__btn">
+          className="result-card__btn">
           Try {tool.tool}
         </a>
       </div>
