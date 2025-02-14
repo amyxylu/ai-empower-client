b2a528bbdc954fb269d69982137c43f934ee4263
Amy <xyl.amy@gmail.com>
2025-02-13 17:07:40 -0800

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<QuizPage />} />
+        <Route path="/results/:resultsId" element={<ResultsPage />} />
+        <Route path="/guide/:guideId" element={<GuidePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
