be05b8b0c3be4782f0a75f9e815fbb7b94c6180c
joiceron <joiceron@hotmail.com>
2025-02-14 01:36:44 -0400

    style footer, add typography variables, update flexbox in header

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,21 +4,20 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer">
-      <h2>Ai Empower</h2>
+      <h2 className="footer__logo">AI Empower</h2>
       <div className="footer__team">
         <h3>Develop by:</h3>
-        <ul>
-          <li>Alma Nursultan</li> 
-          <li>Amy Lu</li>
-          <li>Joice Ceron</li>
-          <li>Mohita Prabhakar</li>
-          <li>Vijay Mariyil Jayan</li>
+        <ul className="footer__team--list">
+          <li className="footer__team--member">Alma Nursultan</li>
+          <li className="footer__team--member">Amy Lu</li>
+          <li className="footer__team--member">Joice Ceron</li>
+          <li className="footer__team--member">Mohita Prabhakar</li>
+          <li className="footer__team--member">Vijay Mariyil Jayan</li>
         </ul>
+        <p className="footer__copy">
+          &copy; 2025 Ai Empower. All rights reserved.
+        </p>
       </div>
-      <p className="footer__text">
-        {" "}
-        &copy; 2025 Ai Empower. All rights reserved.
-      </p>
     </footer>
   );
 };
