362677bc8715ae4689ef37c2ad7d7865c05719fa
Amy <xyl.amy@gmail.com>
2025-02-13 17:58:24 -0800

    revert accidental merge onto main

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
 import "./App.scss";
-import Header from "./components/Header/Header";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import GuidePage from "./pages/GuidePage/GuidePage";
-import QuizPage from "./pages/QuizPage/QuizPage";
-import ResultsPage from "./pages/ResultsPage/ResultsPage";
+
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<QuizPage />} />
-        <Route path="/results/:resultsId" element={<ResultsPage />} />
-        <Route path="/guide/:toolId" element={<GuidePage />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <></>;
 }
+
 export default App;
