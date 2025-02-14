3d30eba4cba895925998b1547efb4f4971febb14
VijayMJ <vijaymj42@gmail.com>
2025-02-13 20:13:57 -0400

    add pages folder and update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.scss";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import GuidePage from "./pages/GuidePage/GuidePage";
+import QuizPage from "./pages/QuizPage/QuizPage";
+import ResultsPage from "./pages/ResultsPage/ResultsPage";
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GuidePage/>} />
+        <Route path="/quizpage" element={<QuizPage/>} />
+        <Route path="/resultspage" element={<ResultsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default App;
